eced8101397fae31c72742ad7125590abde02865
Cat <39655575+KittenMeow@users.noreply.github.com>
2021-06-18 13:15:52 -0400

    Add files via upload

--- gdt.c
+++ gdt.c
@@ -1,5 +1,11 @@
 #include "gdt.h"
 
+struct GDT Null;
+struct GDT KCode;
+struct GDT KData;
+struct GDT UCode;
+struct GDT UData; 
+
 /**
  * \param target A pointer to the 8-byte GDT entry
  * \param source An arbitrary structure describing the GDT entry
@@ -32,4 +38,21 @@ int encodeGdtEntry(uint8_t *target, struct GDT source) {
     target[5] = source.type;
 
     return 0;
+}
+
+void create_descriptor(uint32_t base, uint32_t limit, uint16_t flag) {
+    uint64_t descriptor;
+ 
+    // Create the high 32 bit segment
+    descriptor  =  limit       & 0x000F0000;         // set limit bits 19:16
+    descriptor |= (flag <<  8) & 0x00F0FF00;         // set type, p, dpl, s, g, d/b, l and avl fields
+    descriptor |= (base >> 16) & 0x000000FF;         // set base bits 23:16
+    descriptor |=  base        & 0xFF000000;         // set base bits 31:24
+ 
+    // Shift by 32 to allow for low part of segment
+    descriptor <<= 32;
+ 
+    // Create the low 32 bit segment
+    descriptor |= base  << 16;                       // set base bits 15:0
+    descriptor |= limit  & 0x0000FFFF;               // set limit bits 15:0
 }
